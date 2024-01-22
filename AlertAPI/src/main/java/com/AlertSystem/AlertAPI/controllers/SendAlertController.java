6c2eadcd24fdbbb335dabee697cad0b70d31e514
Nikita <niki.c2000@yandex.ru>
2024-01-22 18:36:48 +0400

    добавлено логирование в бота и AlertAPI

--- AlertAPI/src/main/java/com/AlertSystem/AlertAPI/controllers/SendAlertController.java
+++ AlertAPI/src/main/java/com/AlertSystem/AlertAPI/controllers/SendAlertController.java
@@ -3,6 +3,7 @@ package com.AlertSystem.AlertAPI.controllers;
 
 import com.AlertSystem.AlertAPI.alerUtil.Alerter;
 import com.AlertSystem.AlertAPI.dto.AlertDTO;
+import com.AlertSystem.AlertAPI.util.MyLogger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,19 +13,22 @@ import java.util.Map;
 @RequestMapping("/alert")
 public class SendAlertController {
     private final Alerter alerter;
+    private final MyLogger myLogger;
 
-    public SendAlertController(Alerter alerter) {
+    public SendAlertController(Alerter alerter, MyLogger myLogger) {
         this.alerter = alerter;
+        this.myLogger = myLogger;
     }
 
     @PostMapping("/byRole/{role}")
     public ResponseEntity byRole(@PathVariable String role,
-                                 @RequestBody AlertDTO testDTO) {
+                                 @RequestBody AlertDTO alertDTO) {
+        myLogger.sendInfo("Поступил запрос на оповещение по роли в проекте: " + alertDTO.getIdProject());
         try {
-            alerter.sendAlert(testDTO.getIdProject(),
-                    testDTO.getAlertType(),
-                    testDTO.getMessage(),
-                    testDTO.getAlertPath(),
+            alerter.sendAlert(alertDTO.getIdProject(),
+                    alertDTO.getAlertType(),
+                    alertDTO.getMessage(),
+                    alertDTO.getAlertPath(),
                     role);
             return ResponseEntity.badRequest().body(Map.of("status", "success"));
         } catch (Exception exception) {
@@ -34,12 +38,13 @@ public class SendAlertController {
 
     @PostMapping("/byId/{id}")
     public ResponseEntity byId(@PathVariable int id,
-                                 @RequestBody AlertDTO testDTO) {
+                                 @RequestBody AlertDTO alertDTO) {
+        myLogger.sendInfo("Поступил запрос на оповещение по id в проекте: " + alertDTO.getIdProject());
         try {
-            alerter.sendAlert(testDTO.getIdProject(),
-                    testDTO.getAlertType(),
-                    testDTO.getMessage(),
-                    testDTO.getAlertPath(),
+            alerter.sendAlert(alertDTO.getIdProject(),
+                    alertDTO.getAlertType(),
+                    alertDTO.getMessage(),
+                    alertDTO.getAlertPath(),
                     id);
             return ResponseEntity.ok(Map.of("status", "success"));
         }
